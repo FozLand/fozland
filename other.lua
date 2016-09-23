31abcf37afb1bd7c29abeb8a9f9190db7d4208e0
Foz <fozolo@gmail.com>
2016-09-23 19:30:38 -0400

    Add more complete material chain.
    cobble -> gravel -> sand -> clay -> dirt
    
    The cheap TNT problem should be addressed, but I like the logic of cobble to
    gravel too much to do it here.

--- other.lua
+++ other.lua
@@ -9,14 +9,43 @@ if minetest.get_modpath('bones') then
 	})
 end
 
+-- Make strip mining unnecessary
 minetest.register_craft({
 	type = 'shapeless',
-	output = 'default:sand',
-	recipe = {'default:cobble'}
+	output = 'default:gravel',
+	recipe = {'default:cobble', 'default:cobble'}
 })
 
 minetest.register_craft({
 	type = 'shapeless',
-	output = 'default:dirt 2',
-	recipe = {'default:sand', 'default:sand'}
- })
+	output = 'default:sand',
+	recipe = {'default:gravel', 'default:gravel'}
+})
+
+minetest.register_craft({
+	type = 'shapeless',
+	output = 'default:clay',
+	recipe = {'group:sand', 'group:sand'}
+})
+
+minetest.register_craft({
+	output = 'default:dirt 6',
+	recipe = {
+		{'group:leaves', 'group:leaves', 'group:leaves'},
+	  {'default:clay', 'default:clay', 'default:clay'},
+		{'group:sand',   'group:sand',   'group:sand'  }
+	}
+})
+
+-- Sand to Sand Conversions
+minetest.register_craft({
+	type = 'shapeless',
+	output = 'default:desert_sand',
+	recipe = {'default:sand'}
+})
+
+minetest.register_craft({
+	type = 'shapeless',
+	output = 'default:sand',
+	recipe = {'default:desert_sand'}
+})
