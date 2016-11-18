d8710775da479d36e170ed7e046f5fec9a8e7691
Foz <fozolo@gmail.com>
2016-11-17 17:52:50 -1000

    Whitespace corrections.

--- sawables.lua
+++ sawables.lua
@@ -1,8 +1,8 @@
 if core.global_exists('stairsplus') then
 
 	local dyes = {'white', 'grey', 'black', 'red', 'yellow', 'green', 'cyan',
-								'blue', 'magenta', 'orange', 'violet', 'brown', 'pink',
-								'dark_grey', 'dark_green'}
+	              'blue', 'magenta', 'orange', 'violet', 'brown', 'pink',
+	              'dark_grey', 'dark_green'}
 
 	local mod = ''
 	local types = {}
@@ -31,8 +31,8 @@ if core.global_exists('stairsplus') then
 	-- Coloured Stonebricks
 	mod = 'colouredstonebricks'
 	types = {'black', 'cyan', 'brown', 'dark_blue', 'dark_green', 'dark_grey',
-           'dark_pink', 'green', 'grey', 'orange', 'pink', 'purple', 'red',
-           'white', 'yellow'}
+	         'dark_pink', 'green', 'grey', 'orange', 'pink', 'purple', 'red',
+	         'white', 'yellow'}
 
 	for _, name in pairs(types) do
 		local nodename = mod..':'..name
@@ -50,17 +50,15 @@ if core.global_exists('stairsplus') then
 		def.groups.wool = nil
 		stairsplus:register_all(mod, name, nodename, def)
 	end
-	
+
 	-- Moreores
 	local mod = 'moreores'
 	local types = {'silver_block', 'tin_block', 'mithril_block'}
+
 	for _, name in pairs(types) do
 		local nodename = mod..':'..name
 		local def = table.copy(minetest.registered_nodes[nodename])
 		stairsplus:register_all(mod, name, nodename, def)
 	end
-	
-	
-
 
 end
